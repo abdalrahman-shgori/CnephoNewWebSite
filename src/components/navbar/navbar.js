e45f8bc54b9ad5674b67e5df89e02358f3ecc645
abd <abdalrahman.job9@gmail.com>
2023-12-21 03:10:08 +0300

    contact us page

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -18,6 +18,7 @@ import "../../assets/styles/variables.css";
 import arrowRight from "../../assets/images/arrowRight.svg"
 import LanguageSwitcher from './LanguageSwitcher';
 import "./navbar.css"
+import { useEffect } from 'react';
 const drawerWidth = "100%";
 function NavBar(props) {
     const { t, i18n } = useTranslation();
@@ -30,128 +31,128 @@ function NavBar(props) {
         { title: t("navBar.CAREER"), path: "/career" },
         { title: t("navBar.CONTACTUS"), path: "/Contact-Us" },
     ];
-    const [selectedLink, setSelectedLink] = useState(
-        localStorage.getItem("contact") || 0
-    );    const [mobileOpen, setMobileOpen] = React.useState(false);
+    const [selectedLink, setSelectedLink] = useState("");
+    const [mobileOpen, setMobileOpen] = React.useState(false);
     const [isVisible, setIsVisible] = useState(false);
 
     const { windowMenu } = props;
     const selectLanguage = i18n.language;
-    const handleLinkClick = (index) => {
-        localStorage.setItem("contact", index);
-        setSelectedLink(index); // Update the state with the selected link index
-        if(mobileOpen === true) {
-            handleDrawerToggle()
-        }
 
-    };
 
     const handleDrawerToggle = () => {
         setMobileOpen((prevState) => !prevState);
-        // document.body.style.overflow = isVisible ?'hidden' : "sroll";
-
         window.scroll({
-          top: 0,
-          left: 0,
-          behavior: 'smooth',
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
         });
-      };
-    
-      const handleContactUsClick = () => {
-        // Navigate to the contact us page
-        window.location.href = '/Contact-Us'; // Replace '/contact-us' with the actual URL of your contact us page
-      };
-      const handleBackToHomePage = () => {
-        // Navigate to the home page
-        window.location.href = '/'; // Replace '/contact-us' with the actual URL of your contact us page
-      };
-   
-      const location = useLocation();
-      const isContactUsPage = location.pathname === '/Contact-Us';
+    };
 
-    
+    const handleContactUsClick = () => {
+        // Navigate to the contact us page
+        window.location.href = '/Contact-Us';
+    };
+    const handleBackToHomePage = () => {
+        // Navigate to the home page
+        window.location.href = '/';
+    };
+
+    const location = useLocation();
+    const isContactUsPage = location.pathname === '/Contact-Us';
+
+
+
+
     const drawer = (
         <>
-        <Box
-        sx={{
-            display:"flex",
-            justifyContent:"end"
-        }}
-        >
-
-          <Button
-            sx={{
-                color:"var(--white-color)",
-                fontSize:"24px",
-                paddingLeft: selectLanguage === "en" ? "0px" : "20px",
-                paddingRight: selectLanguage === "ar" ? "0px" : "20px",
-                position:"relative",
-                top:"38px",
-            }}
-            onClick={handleDrawerToggle}
+            <Box
+                sx={{
+                    display: "flex",
+                    justifyContent: "end"
+                }}
             >
-                X
-            </Button>
+
+                <Button
+                    sx={{
+                        color: "var(--white-color)",
+                        fontSize: "24px",
+                        paddingLeft: selectLanguage === "en" ? "0px" : "20px",
+                        paddingRight: selectLanguage === "ar" ? "0px" : "20px",
+                        position: "relative",
+                        top: "38px",
+                    }}
+                    onClick={handleDrawerToggle}
+                >
+                    X
+                </Button>
             </Box>
 
-            <Box sx={{ textAlign: 'center' ,position:"relative",top:"40px",zIndex:"2"}}>
-          <List sx={{ }}>
-              {navItems.map((item, index) => (
-                  <Box
-                      key={index}
-                      sx={{
-                          display: "flex",
-                          flexDirection: "column",
-                          alignItems: "center",
-                          justifyContent: "center",
-                          marginTop: "18px",
-                          padding: "10px",
-                          cursor: "pointer",
-                          textDecoration: "none",
-                      }}
-                      component={Link}
-                      to={item.path}
+            <Box sx={{ textAlign: 'center', position: "relative", top: "40px", zIndex: "2" }}>
+                <List sx={{}}>
+                    {navItems.map((item, index) => (
+                        <Box
+                            key={index}
+                            sx={{
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center",
+                                justifyContent: "center",
+                                marginTop: "18px",
+                                padding: "10px",
+                                cursor: "pointer",
+                                textDecoration: "none",
+                            }}
+                            component={Link}
+                            to={item.path}
 
-                  >
-                      <Typography
-                      sx={{
-                          
-                          fontSize:"24px",
-                          fontWeight:"400",
-                          fontFamily: "var(--English-font)",
-                          color: index === parseInt(selectedLink, 10) ? '#21D6D6' : '#fff',
-                          width:"100%",
-                          height:"100%"
+                        >
+                            <Typography
+                                sx={{
 
-                      }}
-                      onClick={() => handleLinkClick(index)}
+                                    fontSize: "24px",
+                                    fontWeight: "400",
+                                    fontFamily: "var(--English-font)",
+                                    color: item.path === location.pathname ? '#21D6D6' : '#fff',
+                                    width: "100%",
+                                    height: "100%"
 
-                      >
-                          {item.title}
-                      </Typography>
-                  </Box>
+                                }}
+                                onClick={() => handleLinkClick(index)}
 
-              ))}
-          </List>
-      </Box>
+                            >
+                                {item.title}
+                            </Typography>
+                        </Box>
+
+                    ))}
+                </List>
+            </Box>
         </>
-       
+
     );
+
+
+    const handleLinkClick = (index) => {
+        setSelectedLink(index);
+        if (mobileOpen === true) {
+            handleDrawerToggle();
+        }
+    };
     const container = windowMenu !== undefined ? () => window().document.body : undefined;
     return (
         <Box>
             <CssBaseline />
             <AppBar component="nav"
                 sx={{
-                    overflowY:"hidden",
-                    position:"static",
+                    overflowY: "hidden",
+                    position: "static",
                     paddingLeft: selectLanguage === "en" ? { lg: "96px", sm: "6px", xs: "6px" } : { lg: "56px", sm: "29px", xs: "29px" },
                     paddingRight: selectLanguage === "en" ? { lg: "56px", sm: "29px", xs: "29px" } : { lg: "96px", sm: "6px", xs: "6px" },
                     boxShadow: "none",
                     paddingTop: { lg: "26px", sm: "36px", xs: "36px" },
                     paddingBottom: "0px",
                     backgroundColor: "transparent",
-                    fontFamily:"var(--English-font)",
+                    fontFamily: "var(--English-font)",
 
                 }}
             >
@@ -159,12 +160,12 @@ function NavBar(props) {
                     display: "flex",
                     justifyContent: "space-between",
                     padding: "0px 0px",
-                    width:"100%",
+                    width: "100%",
 
                 }}>
                     <Box className="logoGap" sx={{ display: "flex", alignItems: "center" }}>
 
-                        <img style={{cursor:"pointer"}} onClick={handleBackToHomePage} src={Logo}></img>
+                        <img style={{ cursor: "pointer" }} onClick={handleBackToHomePage} src={Logo}></img>
 
                         <Box sx={{ display: { lg: "flex", xs: 'none', sm: 'none' } }}>
                             {navItems.slice(1, 6).map((items, index) => (
@@ -176,7 +177,7 @@ function NavBar(props) {
                                     sx={{
                                         display: "flex",
                                         color: index === selectedLink ? '#21D6D6' : '#fff',
-                                        transition: 'color 0.3s ease', 
+                                        transition: 'color 0.3s ease',
                                         marginRight: '8px',
                                         width: "115px",
                                         textAlign: "center",
@@ -186,9 +187,9 @@ function NavBar(props) {
                                         lineHeight: "36px",
                                         fontFamily: "var(--English-font)",
                                         cursor: "pointer",
-                                        textTransform:"uppercase",
-                                        textDecoration:"none"
-                                        
+                                        textTransform: "uppercase",
+                                        textDecoration: "none"
+
                                     }}
                                     onClick={() => handleLinkClick(index)}
                                 >
@@ -200,15 +201,15 @@ function NavBar(props) {
                     <Box sx={{
                         display: "flex",
                         gap: { lg: "24px", xs: "0px" },
-                        width:"auto"
-                     
+                        width: "auto"
+
                     }}>
                         <LanguageSwitcher />
                         <Box sx={{
                             display: { lg: "block", xs: 'none', sm: 'none' },
-                            cursor:"pointer"
+                            cursor: "pointer"
                         }}
-                        onClick={handleContactUsClick}
+                            onClick={handleContactUsClick}
 
                         >
                             <Box
@@ -228,8 +229,8 @@ function NavBar(props) {
                                         color: "var(--white-color)",
                                         padding: "6px 0px",
                                         whiteSpace: "nowrap",
-                                        fontWeight:"600",
-                                        fontFamily:"var(--English-font)",
+                                        fontWeight: "600",
+                                        fontFamily: "var(--English-font)",
                                         '&:hover': {
                                             backgroundColor: 'transparent', // Set the background color to transparent or any value you want
                                         },
@@ -248,8 +249,8 @@ function NavBar(props) {
 
                         </Box>
                         <IconButton
-                        // disableScrollLock
-                         className={`scroll-to-top-button ${isVisible ? 'visible' : ''}`}
+                            // disableScrollLock
+                            className={`scroll-to-top-button ${isVisible ? 'visible' : ''}`}
                             aria-label="open drawer"
                             onClick={handleDrawerToggle}
                             sx={{
@@ -258,10 +259,10 @@ function NavBar(props) {
                                 padding: "0px",
                                 paddingLeft: selectLanguage === "en" ? "19px" : "0px",
                                 paddingRight: selectLanguage === "ar" ? "19px" : "0px",
-                                zIndex:"0"
+                                zIndex: "0"
                             }}
                             disableRipple
-                            
+
                         >
                             <MenuIcon />
                         </IconButton>
@@ -280,7 +281,7 @@ function NavBar(props) {
                             invisible: true,
                         },
                     }}
-                    sx={{ 
+                    sx={{
 
                         display: { xs: 'block', sm: 'block' },
                         '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth, marginTop: "0px", backgroundColor: "var(--website-bg-color)", boxShadow: "none", color: "#fff", },
@@ -288,12 +289,12 @@ function NavBar(props) {
                 >
                     {drawer}
                 </Drawer>
-                
+
             </nav>
-            <Box component="main"  sx={{paddingBottom:"20px",display:"none"}}>
-                <Toolbar/>
+            <Box component="main" sx={{ paddingBottom: "20px", display: "none" }}>
+                <Toolbar />
             </Box>
-           
+
         </Box>
     );
 }
